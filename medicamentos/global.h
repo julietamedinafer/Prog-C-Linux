dcbd9d95ef79e84235c0b6b83d194bcf566f1d67
Julieta Medina <118027608+julietamedinafer@users.noreply.github.com>
2026-05-05 17:23:53 -0300

    Delete medicamentos directory

--- medicamentos/global.h
+++
@@ -1,8 +0,0 @@
-#ifndef GLOBAL_H
-#define GLOBAL_H
-
-
-extern int id_semaforo;
-extern char linea[256];
-
-#endif
--- medicamentos/readme.txt
+++
@@ -1,4 +0,0 @@
-Orden de ejecucion:
-
-1) ./control
-2) ./medicamento + parametro (1, 2 o 3)
