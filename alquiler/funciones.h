a0ca75e08e5ec20bfcb7ec9853a5462b80eec2e3
Julieta Medina <118027608+julietamedinafer@users.noreply.github.com>
2026-05-05 17:23:26 -0300

    Delete alquiler directory

--- alquiler/funciones.h
+++
@@ -1,8 +0,0 @@
-#ifndef FUNCIONES_H
-#define FUNCIONES_H
-
-void inicializarAleatorio(void);
-int generarAleatorio(int desde, int hasta);
-/*void menu(int op,char *mensaje[]);*/
-
-#endif
--- alquiler/readme.txt
+++
@@ -1,5 +0,0 @@
-Orden de ejecucion:
-
-1) make
-2) ./sistema
-3) ./interesado + parametro (1, 2 o 3). Ejemplo: ./interesado 1
